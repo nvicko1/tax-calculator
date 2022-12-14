0e5633736c9d1d4f8b005425bc35c7f081a9898c
nvicko1 <nikolavicentijevic011@gmail.com>
2022-12-14 23:46:29 +0100

    Changed_webpack_config

--- webpack.config.production.js
+++ webpack.config.production.js
@@ -1,13 +1,14 @@
-const HtmlWebPackPlugin = require("html-webpack-plugin");
-const path = require("path");
+const HtmlWebPackPlugin = require('html-webpack-plugin')
+const path = require('path')
+
 const htmlPlugin = new HtmlWebPackPlugin({
-  template: "./src/index.html",
-  filename: "./index.html",
-  publicPath: "/",
-});
+  template: './src/index.html',
+  filename: './index.html',
+  publicPath: '/',
+})
 
 module.exports = {
-  mode: "production",
+  mode: 'development',
   devServer: {
     magicHtml: true,
     historyApiFallback: true,
@@ -18,24 +19,24 @@ module.exports = {
         test: /\.js$/,
         exclude: /node_modules/,
         use: {
-          loader: "babel-loader",
+          loader: 'babel-loader',
         },
       },
       {
         test: /\.jsx$/,
         exclude: /node_modules/,
         use: {
-          loader: "babel-loader",
+          loader: 'babel-loader',
         },
       },
       {
         test: /\.css$/,
-        use: ["style-loader", "css-loader", "postcss-loader"],
+        use: ['style-loader', 'css-loader', 'postcss-loader'],
       },
     ],
   },
   resolve: {
-    extensions: [".js", ".jsx"],
+    extensions: ['.js', '.jsx'],
   },
   plugins: [htmlPlugin],
-};
+}
